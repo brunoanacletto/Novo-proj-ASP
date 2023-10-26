fb781cd30527fab6087b1b68371d5348c80c9976
brunoanacletto <103295971+brunoanacletto@users.noreply.github.com>
2023-10-26 17:16:17 -0300

    Novo Proj ASP

---
+++ Consulta.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp01.Models
+{
+    public class Consulta
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set;}
+    }
+}
